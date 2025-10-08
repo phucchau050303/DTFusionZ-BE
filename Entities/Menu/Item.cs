a3e13e98cf04eabc4807b512ce4307871240c669
Hoang Bao Phuc Chau <phcchu88@gmail.com>
2025-10-09 10:47:17 +1100

    add Image Url property to Item class

--- Entities/Menu/Item.cs
+++ Entities/Menu/Item.cs
@@ -6,6 +6,8 @@ namespace DTFusionZ_BE.Entities
 
         public string Name { get; set; } = string.Empty;
 
+        public string ImageUrl { get; set; } = string.Empty;
+
         public string Description { get; set; } = string.Empty; 
 
         public decimal Price { get; set; }
